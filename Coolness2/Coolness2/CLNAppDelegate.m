031014632f9288104f3e177eabf9488167353e2e
Jonathan Lehr <jonathan@aboutobjects.com>
2020-04-02 15:58:38 -0400

    Added Coolness2

---
+++ Coolness2/Coolness2/CLNAppDelegate.m
@@ -0,0 +1,18 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "CLNAppDelegate.h"
+#import "CLNCoolViewController.h"
+
+@implementation CLNAppDelegate
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+    self.window = [[UIWindow alloc] init];
+    self.window.backgroundColor = UIColor.systemYellowColor;
+    
+    self.window.rootViewController = [[CLNCoolViewController alloc] initWithNibName:@"CoolStuff" bundle:nil];
+    
+    [self.window makeKeyAndVisible];
+}
+
+@end
